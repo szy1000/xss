c933c02dd80634027bff784175e79fbf2c7b10be
szy1000 <1538078053@qq.com>
2017-05-31 17:59:24 +0800

    update express@3 to express@4

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 var express = require('express');
-var router = new express.Router();
-
-var comments = {};
-
+var router = express.Router();
 
 function html_encoude(str){
 	var s = '';
@@ -17,29 +14,21 @@ function html_encoude(str){
 	s = s.replace(/\n/g,"<br>");
 	return s;
 }
-/*
- * GET home page.
- */
 
-// exports.index = function(req, res){
-//   res.render('index', { title: 'Express' });
-// };
-router.get('/', function(req,res,next){
-	res.render('index', { title: 'Express' })
-})
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 router.get('/comment', function(req,res,next){
 	comments.v = html_encode(req.query.comment);
-});
+})
 
 router.get('/getComment', function(req,res,next){
 	res.json({
 		comment:comments.v,
+		
 	})
-});
+})
 
 module.exports = router;
-
-// module.exports = function(req,res){
-// 	res.send("respond with a resource");
-// }
